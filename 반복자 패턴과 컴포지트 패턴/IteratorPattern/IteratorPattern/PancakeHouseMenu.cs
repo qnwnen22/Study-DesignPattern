92e6a0034c69a6c3183ef2331b8445795ab81b86
moka <qnwnen0228@gmail.com>
2023-10-30 13:49:55 +0900

    초기 커밋.

---
+++ 반복자 패턴과 컴포지트 패턴/IteratorPattern/IteratorPattern/PancakeHouseMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    public class PancakeHouseMenu
+    {
+        public List<MenuItem> MenuItems { get; private set; } = new List<MenuItem>();
+        public PancakeHouseMenu()
+        {
+            AddItem("K&B 팬케이크 세트", "스크램블 에그와 토스트가 곁들여진 팬케이크", true, 2.99d);
+            AddItem("레귤러 팬케이크 세트", "달걀 프라이와 소시기자 곁들여진 팬케이크", true, 2.99d);
+            AddItem("", "", true, 2.99d);
+            AddItem("", "", true, 2.99d);
+        }
+
+        public void AddItem(string name, string description, bool vegetarian, double price)
+        {
+            MenuItems.Add(new MenuItem(name, description, vegetarian, price));
+        }
+    }
+}
